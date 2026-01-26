e811eac9cb87b8598314d65f40472fe39955171a
tankoid2-del <>
2026-01-26 14:14:17 +0000

    Index Iteration

--- loops/index_iteration/main.py
+++ loops/index_iteration/main.py
@@ -1,12 +1,20 @@
 prices = [29.99, 45.50, 12.75, 38.20]
-discounts = [0.10, 0.20, 0.15, 0.05]
-#discount_two = 0.20
-#discount_tree = 0.15
-#discount four = 0.05
+discount_rate = [0.90, 0.80, 0.85, 0.95]
+#discount_one = 0.90
+#discount_two = 0.80
+#discount_tree = 0.85
+#discount_four = 0.95
 for cost in range(len(prices)):
-    if prices[cost] == 0
-    
-    updated_price = 
-    print(f"Updated price for item {cost}: ${prices[cost]}")
+    if cost == 0:
+        discount = 0.10
+    elif cost == 1:
+        discount = 0.20
+    elif cost == 2:
+        discount = 0.15
+    elif cost == 3:
+        discount = 0.05
+    prices[cost] = prices[cost] * (1- discount)
+    print(f"Updated price for item {cost}: ${prices[cost]:.2f}")
 
-print()
+    #print("new price:", {prices[cost]} * range(discounts)[cost])
+    #print("new price:",prices)
