06670e6f54b7d53d06340337506d55b6bc932f23
tankoid2-del <>
2026-01-26 15:36:11 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,8 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = grocery_items[0:4]
+dairy2 = grocery_items[5:11]
+dairy3 = grocery_items[12:17]
+dairy4 = grocery_items[18:24]
+dairy5 = grocery_items[25:32]
+dairy6 = grocery_items[33:40]
+print("We have dairy and bakery items:", dairy1 + ", " + dairy2 + ", and " + dairy3 ,"in aisle 5")
