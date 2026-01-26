3d9513ac1db02f5a60c87cc82223897c28f49959
tankoid2-del <>
2026-01-26 15:33:46 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,9 @@
+Item_name = "Bread"
+item_price = 4.52
+items_in_stock = 230
+
+
+# Testing
+print("Item:", Item_name)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", items_in_stock)
