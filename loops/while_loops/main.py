8d384380f494a8925cfef2a44c5a83cb44728caf
tankoid2-del <>
2026-01-26 16:34:07 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,8 @@
+start_number = 5
+countdown_values = []
+while start_number >= 1:
+    countdown_values.append(start_number)
+    start_number-=1
+print("Discount countdown complete!")
+print(countdown_values)
+    
